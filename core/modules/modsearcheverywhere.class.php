3bff64482a0c3330d5279e0e22f0ffab83baeafc
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2024-07-26 09:53:48 +0200

    fix pr

--- core/modules/modsearcheverywhere.class.php
+++ core/modules/modsearcheverywhere.class.php
@@ -158,7 +158,7 @@ class modsearcheverywhere extends DolibarrModules
         // 'categories_x'		to add a tab in category view
         // (replace 'x' by type of category (0=product, 1=supplier, 2=customer, 3=member)
         // Dictionnaries
-        if (isModEnabled('searcheverywhere')) {
+        if (! isModEnabled('searcheverywhere')) {
             $conf->searcheverywhere=new stdClass();
             $conf->searcheverywhere->enabled = 0;
         }
